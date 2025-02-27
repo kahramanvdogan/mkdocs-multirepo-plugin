429e83069a4d66a1b1546702fc43e131ce49477a
Kahraman V Dogan <kahraman.v.dogan@akerbp.com>
2025-02-27 15:31:08 +0100

    Make it possible to use more than one code platform at the same time

--- mkdocs_multirepo_plugin/scripts/sparse_clone.sh
+++ mkdocs_multirepo_plugin/scripts/sparse_clone.sh
@@ -10,16 +10,18 @@ dirs=( "$@" )
 protocol="$(echo "$url" | sed 's/:\/\/.*//')"
 url_rest="$(echo "$url" | sed 's/.*:\/\///')"
 
-if [[ -n  "$AccessToken" ]]; then
+
+if [[ $url_rest == *'azure.com'* && -n  "$AccessToken" ]]; then
     url_to_use="${protocol}://$AccessToken@$url_rest"
     git config http.extraheader "AUTHORIZATION: bearer $AccessToken"
-elif [[ -n  "$GithubAccessToken" ]]; then
+elif [[ $url_rest == *'github.com'* && -n "$GithubAccessToken" ]]; then
     url_to_use="${protocol}://x-access-token:$GithubAccessToken@$url_rest"
-elif [[ -n  "$GitlabCIJobToken" ]]; then
+elif [[ $url_rest == *'gitlab.com'* && -n  "$GitlabCIJobToken" ]]; then
     url_to_use="${protocol}://gitlab-ci-token:$GitlabCIJobToken@$url_rest"
 else
   url_to_use="$url"
 fi
+
 
 git clone --branch "$branch" --depth 1 --filter=blob:none --sparse $url_to_use "$name" || exit 1
 cd "$name"
